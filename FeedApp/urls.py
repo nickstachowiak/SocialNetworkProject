540a78ba9fbd20c3cd1529f7990e2d77d25a15ae
Nick Stachowiak <nick_stachowiak>
2022-05-02 15:04:55 -0500

    First Video

--- FeedApp/urls.py
+++ FeedApp/urls.py
@@ -5,7 +5,9 @@ app_name = 'FeedApp'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    
+    path('profile/', views.profile, name='profile'),
+    path('myfeed',views.myfeed, name='myfeed'),
+
     ]
 
     
